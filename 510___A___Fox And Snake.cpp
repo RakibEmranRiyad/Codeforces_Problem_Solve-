8418214db1b1849138d1fc6c1d4750f39a4a97e3
S. M. Rakib Emran Riyad <83816259+RakibEmranRiyad@users.noreply.github.com>
2021-05-27 18:36:54 +0600

    Add files via upload

---
+++ 510___A___Fox And Snake.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,m,i,j;
+
+    cin>>n>>m;
+
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            if(i%2==0)
+            {
+                cout<<"#";
+            }
+            else if((i-1)%4==0 && j==m-1)
+            {
+                    cout<<"#";
+            }
+            else if((i+1)%4==0 && j==0)
+            {
+               cout<<"#";
+            }
+            else
+                cout<<".";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
