0a77b9dc4875b5c1cabb33d0e71addafaf9d5a06
S. M. Rakib Emran Riyad <83816259+RakibEmranRiyad@users.noreply.github.com>
2021-05-30 12:47:43 +0600

    Add files via upload

---
+++ 155___A___I_love_HP-PC.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,n,c=0;
+
+    cin>>n;
+    int a[n];
+
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+
+    int max=a[0];
+    int min=a[0];
+
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+            c++;
+        }
+        if(a[i]<min)
+        {
+            min=a[i];
+            c++;
+        }
+    }
+
+    cout<<c<<endl;
+
+
+
+    return 0;
+}
